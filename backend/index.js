729205605ce684cf1d0499c6e88ec2cf7ef140b0
mohamedyusuf787 <mohamedyusuf13m2003@gmail.com>
2026-04-16 10:48:52 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://mohamedyusuf_db_user:ibTVxIa11jtUjaz7@cluster0cluster0.yttasfv.mongodb.net/passkey")
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
   console.log("Connected with mongodb");
 }).catch((err) => {
